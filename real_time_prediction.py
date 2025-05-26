291431d333b11c4fd6cd7d9e6bb4ed85ca9489b0
Loran-ash <aaaloran383@gmail.com>
2025-05-26 23:59:13 +0800

    新增使用cv2的即時影像辨識，但是和gradio尚未串接在一起。新增的檔案有使用方式範例

---
+++ real_time_prediction.py
@@ -0,0 +1,80 @@
+import cv2
+from ultralytics import YOLO
+from gradio_webrtc import WebRTC
+import math
+import time
+
+
+model = YOLO("./trained_model/best.pt")
+
+
+def real_time_predict(model, classNames, cap=cv2.VideoCapture(0)):
+
+    #cap = cv2.VideoCapture(0)
+    # cap.set(3, 640)
+    # cap.set(4, 480)
+
+    #while True:
+    ret, img = cap.read()
+    results = model(img, stream=True)
+
+    for r in results:
+        boxes = r.boxes
+
+        for box in boxes:
+            # bounding box
+            x1, y1, x2, y2 = box.xyxy[0]
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2) # convert to int values
+
+            # put box in cam
+            cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+
+            # confidence
+            confidence = math.ceil((box.conf[0]*100))/100
+            print("Confidence --->",confidence)
+
+            # class name
+            cls = int(box.cls[0])
+            print("Class name -->", classNames[cls])
+
+            # object details
+            org = [x1, y1]
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            fontScale = 1
+            color = (255, 0, 0)
+            thickness = 2
+
+            cv2.putText(img, classNames[cls], org, font, fontScale, color, thickness)
+    cv2.imshow('Webcam', img)  #不想顯示畫面，可以註解掉這行
+
+    if cv2.waitKey(1) == ord('q'):
+        return
+
+    # cap.release()
+    # cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    cap=cv2.VideoCapture(0)
+    cap.set(3, 640)
+    cap.set(4, 480)
+    classNames =["circle_1", "circle_2", "circle_3", "circle_4", "circle_5", "circle_6", "circle_7", "circle_8", "circle_9",
+                "bamboo_1", "bamboo_2", "bamboo_3", "bamboo_4", "bamboo_5", "bamboo_6", "bamboo_7", "bamboo_8", "bamboo_9",
+                "character_1", "character_2", "character_3", "character_4", "character_5", "character_6", "character_7", "character_8", "character_9",
+                "north", "south", "west", "east", "green", "red", "white" ]
+    # classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
+    #             "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
+    #             "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
+    #             "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
+    #             "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
+    #             "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
+    #             "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
+    #             "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
+    #             "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
+    #             "teddy bear", "hair drier", "toothbrush"
+    #             ]
+    while True:
+        real_time_predict(model, classNames, cap)
+        # time.sleep(1)
+        
+    cap.release()
+    cv2.destroyAllWindows()
